c5e0c5d98b88fc70828dec2cf51d13340ad239ad
Oyiga Victor <victoroyiga01@gmail.com>
2024-07-26 12:52:40 +0100

    affirtation page displaying

--- app/(tabs)/affirmation.tsx
+++ app/(tabs)/affirmation.tsx
@@ -1,12 +1,29 @@
-import { View, Text } from 'react-native'
+import { View, Text , ScrollView} from 'react-native'
 import React from 'react'
+import AppGradient from '@/components/AppGradient'
+import AFFIRMATION_GALLERY from "@/constants/affirmation-gallery"
+import GuidedAffirmatioonGallery from '@/components/GuidedAffirmatioonGallery'
 
 const affirmation = () => {
-  return (
-    <View>
-      <Text>affirmation</Text>
-    </View>
-  )
+    return (
+        <View className='flex-1 '>
+            <AppGradient  colors={["#2e1f58", "#344266","#a790af"]}>
+                <ScrollView showsVerticalScrollIndicator={false}>
+                   <Text className='text-zinc-50 text-2xl font-bold mt-10'>Change Your beliefs with affirmation</Text>
+
+                   <View>
+                     {AFFIRMATION_GALLERY.map((g)=>(
+                        <GuidedAffirmatioonGallery 
+                           key={g.title}
+                           title={g.title}
+                           previews={g.data}
+                        />
+                     ))}
+                   </View>
+                </ScrollView>
+            </AppGradient>
+        </View>
+    )
 }
 
 export default affirmation
---
+++ constants/models/AffirmationCategory.ts
@@ -0,0 +1,11 @@
+export interface AffirmationsCategory{
+    title: string;
+    data: GalleryPreiewData[];
+}
+
+
+export interface GalleryPreiewData{
+    id: number;
+    text: string;
+    image: any;
+}
