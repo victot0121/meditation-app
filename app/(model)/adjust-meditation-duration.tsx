a428e57d672a5abfc492fa51c5d95209b9577f6c
victor oyiga <victoroyiga01@gmail.com>
2024-08-05 13:58:39 +0100

    adjust-meditaton-duration: change the duration word to minutes word

--- app/(model)/adjust-meditation-duration.tsx
+++ app/(model)/adjust-meditation-duration.tsx
@@ -18,21 +18,21 @@ const AdjustMeditationDuration = () => {
   return (
     <View className='flex-1 relative'>
       <AppGradient colors={["#161b2e", "#0a4d4a", "#766667"]}>
-        <Text>Welcome to adject meditation duration</Text>
         <Pressable onPress={() => router.back()} className='absolute top-12 left-6 z-10'>
           <AntDesign name='leftcircleo' size={50} color="white" />
         </Pressable>
         <View className='justify-center h-4/5 mt-28'>
+          <Text className='text-white mt-3 mb-6 ml-9'>Welcome to adject meditation duration</Text>
           <Text className='text-center font-bold text-3xl text-white  mb-8'>
             Adjust your meditation duration
           </Text>
           <View>
 
-             <CustomButton title='10 button' onPress={()=>  handlePress(10)} containerStyles='m-5'/>
-             <CustomButton title='5 button' onPress={()=>  handlePress(5 * 60)} containerStyles='m-5'/>
-             <CustomButton title='10 button' onPress={()=>  handlePress(10 * 60)} containerStyles='m-5'/>
-             <CustomButton title='15 button' onPress={()=>  handlePress(15 * 60)} containerStyles='m-5'/>
-             <CustomButton title='20 button' onPress={()=>  handlePress(20  * 60)} containerStyles='m-5'/>
+             <CustomButton title='10 minutes' onPress={()=>  handlePress(10)} containerStyles='m-5'/>
+             <CustomButton title='5 minutes' onPress={()=>  handlePress(5 * 60)} containerStyles='m-5'/>
+             <CustomButton title='10 minutes' onPress={()=>  handlePress(10 * 60)} containerStyles='m-5'/>
+             <CustomButton title='15 minutes' onPress={()=>  handlePress(15 * 60)} containerStyles='m-5'/>
+             <CustomButton title='20 minutes' onPress={()=>  handlePress(20  * 60)} containerStyles='m-5'/>
   
           </View>
         </View>
